aac65f8378adefe03bdcf9a058938f0ab1fb34d2
Ray <megrxu@proton.me>
2023-07-23 16:09:39 +0800

    Typo headers

--- src/routes/api/upload/+server.ts
+++ src/routes/api/upload/+server.ts
@@ -1,15 +1,25 @@
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+    return json({
+        message: 'You can only use POST method in this endpoint.'
+    })
+}
+
 export async function POST({ fetch, request, platform }) {
     if (platform) {
         const UPLOAD_URL = `https://api.cloudflare.com/client/v4/accounts/${platform.env.ACCOUNT_ID}/images/v1`
         const req = {
             method: 'POST',
-            header: {
+            headers: {
                 'Authorization': `Bearer ${platform.env.CF_IMAGES_API_KEY}`
             },
             body: request.body,
         }
         return fetch(UPLOAD_URL, req);
     } else {
-        return new Response("401 Unauthorized.")
+        return json({
+            message: '401 Unauthorized.'
+        })
     }
 }
