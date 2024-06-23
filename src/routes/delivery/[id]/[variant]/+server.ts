88df9e288b09700bd2775f8a2820088aaf8eb5b8
Ray <megrxu@proton.me>
2024-06-23 21:24:47 +0800

    fix: variant cache path

--- src/routes/delivery/[id]/[variant]/+server.ts
+++ src/routes/delivery/[id]/[variant]/+server.ts
@@ -1,9 +1,8 @@
 import { SERVER_URL, S3_PUBLIC_ACCESS_ENDPOINT } from '$env/static/private';
 
-
 export async function GET({ fetch, params: { id, variant } }) {
-    if (S3_PUBLIC_ACCESS_ENDPOINT != '') {
-        let response = await fetch(`${S3_PUBLIC_ACCESS_ENDPOINT}/${id}/${variant}`);
+    if (S3_PUBLIC_ACCESS_ENDPOINT != '' && variant != 'original') {
+        let response = await fetch(`${S3_PUBLIC_ACCESS_ENDPOINT}/cache/public_${id}_${variant}.JPEG`);
         if (response.status == 200) {
             return response;
         } else {
